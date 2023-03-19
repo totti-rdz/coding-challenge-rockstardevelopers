f942998d92833f1c653585693de421b6a27cf7e1
Michael Sindlinger <michael@rockstardevelopers.de>
2023-03-19 06:41:31 +0100

    init commit

---
+++ Readme.md
@@ -0,0 +1,67 @@
+# Zeiterfassungs-App-API
+
+Dieses Projekt ist eine einfache API für eine Zeiterfassungs-App, die in TypeScript geschrieben ist. Die API unterstützt das Anlegen von Projekten, Mitarbeitern und das Erfassen von Arbeitszeiten.
+Systemanforderungen
+
+    Node.js (v14 oder höher)
+    npm
+
+## Installation
+
+	Klone das Repository oder lade den Quellcode herunter und entpacke es.
+	Öffne einen Terminal und navigiere zum Hauptverzeichnis des Projekts.
+	Führe npm install aus, um alle benötigten Abhängigkeiten zu installieren.
+
+## Ausführen der Anwendung
+
+Führe im Hauptverzeichnis des Projekts den Befehl npm start aus. Der Server startet auf Port 3000 oder dem in der Umgebungsvariable PORT angegebenen Port.
+API-Endpunkte
+
+Projekte
+
+    POST /projects: Erstellt ein neues Projekt.
+
+    Request-Struktur:
+
+    json
+
+    {
+      "name": "Beispielprojekt",
+      "client": "Beispielkunde"
+    }
+
+Mitarbeiter
+
+    POST /employees: Erstellt einen neuen Mitarbeiter.
+
+    Request-Struktur:
+
+    json
+
+    {
+      "firstName": "Max",
+      "lastName": "Mustermann",
+      "email": "max.mustermann@example.com"
+    }
+
+Zeitbuchungen
+
+    POST /timeEntries: Erfasst Arbeitszeit für einen Mitarbeiter.
+
+    Request-Struktur:
+
+    json
+
+    {
+      "employeeId": "EMPLOYEE_ID",
+      "projectId": "PROJECT_ID",
+      "task": "Beispielaufgabe",
+      "minutes": 120
+    }
+
+## Anpassungen
+
+Um weitere Funktionen oder Anpassungen an der API vorzunehmen, ändere den TypeScript-Code in der Datei src/server.ts oder füge weitere Dateien hinzu. Vergiss nicht, TypeScript zu kompilieren, bevor du die Anwendung erneut ausführst, indem du tsc ausführst oder die ts-node-Befehlszeile verwendest.
+Lizenz
+
+Dieses Projekt ist lizenzfrei.
---
+++ src/server.ts
@@ -0,0 +1,87 @@
+// src/server.ts
+import express, { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+
+const app = express();
+app.use(express.json());
+
+interface Project {
+  id: string;
+  name: string;
+  client: string;
+}
+
+interface Employee {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+interface TimeEntry {
+  id: string;
+  employeeId: string;
+  projectId: string;
+  task: string;
+  minutes: number;
+}
+
+const dataFile = "./data.json";
+
+const loadData = (): { projects: Project[]; employees: Employee[]; timeEntries: TimeEntry[] } => {
+  try {
+    const dataBuffer = fs.readFileSync(dataFile);
+    const dataJson = dataBuffer.toString();
+    return JSON.parse(dataJson);
+  } catch (error) {
+    return {
+      projects: [],
+      employees: [],
+      timeEntries: [],
+    };
+  }
+};
+
+const saveData = (data: { projects: Project[]; employees: Employee[]; timeEntries: TimeEntry[] }): void => {
+  const dataJson = JSON.stringify(data);
+  fs.writeFileSync(dataFile, dataJson);
+};
+
+app.post("/projects", (req: Request, res: Response) => {
+  const { name, client } = req.body;
+  const newProject: Project = { id: uuidv4(), name, client };
+  const data = loadData();
+  data.projects.push(newProject);
+  saveData(data);
+  res.status(201).json(newProject);
+});
+
+app.post("/employees", (req: Request, res: Response) => {
+  const { firstName, lastName, email } = req.body;
+  const newEmployee: Employee = { id: uuidv4(), firstName, lastName, email };
+  const data = loadData();
+  data.employees.push(newEmployee);
+  saveData(data);
+  res.status(201).json(newEmployee);
+});
+
+app.post("/timeEntries", (req: Request, res: Response) => {
+  const { employeeId, projectId, task, minutes } = req.body;
+  const newTimeEntry: TimeEntry = {
+    id: uuidv4(),
+    employeeId,
+    projectId,
+    task,
+    minutes,
+  };
+  const data = loadData();
+  data.timeEntries.push(newTimeEntry);
+  saveData(data);
+  res.status(201).json(newTimeEntry);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
